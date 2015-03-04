9b6b5a060beae7ef5a11e9327bf159699bb92f0a
Jack Zaharoff <jackzaharoff@gmail.com>
2015-03-03 23:01:44 -0500

    Move email settings to initializer.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,16 +79,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'entapp.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings = {
-      address: 'smtp.sendgrid.net',
-      port:     '587',
-      authentication: :plain,
-      user_name:  ENV['SMTP_USERNAME'],
-      password: ENV['SMTP_PASSWORD'],
-      domain: 'heroku.com',
-      enable_starttls_auto: true
-  }
+
 
   config.consider_all_requests_local = true
 end
