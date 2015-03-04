bc4ddedf31e8a5249c9f5eea2da13da8f541277b
Jack Zaharoff <jackzaharoff@gmail.com>
2015-03-03 22:39:41 -0500

    Sendgrid mail setup

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,10 +1,1 @@
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = {
-    address: 'smtp.sendgrid.net',
-    port:     '587',
-    authentication: :plain,
-    user_name:  ENV['SMTP_USERNAME'],
-    password: ENV['SMTP_PASSWORD'],
-    domain: 'heroku.com',
-    enable_starttls_auto: true
-}
+
